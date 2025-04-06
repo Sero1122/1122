97a25b8ba699cc9c29b2aa40e13878d853919234
Sero1122 <sergejgabaraev3@gmail.com>
2025-04-06 07:44:30 +0000

    Fixed quest3.sh to correctly set permissions and create important_data_for_ai_module_2.txt

---
+++ src/quest3.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Даем права на выполнение ai_initial_module.sh
+chmod +x ai_initial_module.sh
+
+# Даем права на выполнение ai_module_2.sh
+chmod +x ai_module_2.sh
+
+# Создаем правильное содержимое файла important_data_for_ai_module_2.txt
+echo "1" > important_data_for_ai_module_2.txt
+echo "2" >> important_data_for_ai_module_2.txt
+echo "3" >> important_data_for_ai_module_2.txt
+echo "4" >> important_data_for_ai_module_2.txt
+echo "5" >> important_data_for_ai_module_2.txt
